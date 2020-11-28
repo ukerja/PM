f6b8afb5e5651c83b31c293eb663d928db67239a
wavedeveloper <nikolas.yoshi8888@gmail.com>
2020-11-28 12:05:07 +0700

    update

--- pm.sql
+++ pm.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 25, 2020 at 09:32 AM
--- Server version: 10.4.11-MariaDB
--- PHP Version: 7.4.6
+-- Generation Time: Nov 28, 2020 at 06:04 AM
+-- Server version: 10.4.13-MariaDB
+-- PHP Version: 7.2.31
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -42,7 +42,18 @@ INSERT INTO `ambil_minyak` (`id`, `nama_nelayan`, `tanggal`, `jumlah`) VALUES
 (1, 'madi', '2020-03-01', 100),
 (2, 'aris', '2020-09-03', 100),
 (3, 'aris', '2020-12-09', 100),
-(4, 'madi', '2020-03-03', 10);
+(4, 'madi', '2020-03-03', 10),
+(18, 'arisman', '2020-02-01', 10),
+(19, 'aris', '2020-04-04', 100),
+(20, 'aris', '2020-02-02', 800);
+
+--
+-- Triggers `ambil_minyak`
+--
+DELIMITER $$
+CREATE TRIGGER `INSERT_TO_STOCK_IN` AFTER INSERT ON `ambil_minyak` FOR EACH ROW INSERT INTO stock_in (tanggal, jumlah, status) VALUES (new.tanggal, new.jumlah, "out")
+$$
+DELIMITER ;
 
 -- --------------------------------------------------------
 
@@ -86,7 +97,10 @@ INSERT INTO `nelayan` (`id`, `nama`) VALUES
 (5, 'sss'),
 (7, 'madi'),
 (9, 'rando'),
-(10, '');
+(10, ''),
+(11, 'nor'),
+(12, 'arisman'),
+(13, 'maman');
 
 -- --------------------------------------------------------
 
@@ -96,17 +110,20 @@ INSERT INTO `nelayan` (`id`, `nama`) VALUES
 
 CREATE TABLE `stock_in` (
   `id` int(11) NOT NULL,
-  `nama_nelayan` varchar(20) NOT NULL,
   `tanggal` date NOT NULL,
-  `jumlah` float NOT NULL
+  `jumlah` float NOT NULL,
+  `status` varchar(10) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `stock_in`
 --
 
-INSERT INTO `stock_in` (`id`, `nama_nelayan`, `tanggal`, `jumlah`) VALUES
-(1, 'madi', '2020-11-25', 1000);
+INSERT INTO `stock_in` (`id`, `tanggal`, `jumlah`, `status`) VALUES
+(2, '2020-04-02', 1000, 'in'),
+(3, '2020-02-01', 10, 'out'),
+(4, '2020-04-04', 100, 'out'),
+(5, '2020-02-02', 800, 'out');
 
 --
 -- Indexes for dumped tables
@@ -144,7 +161,7 @@ ALTER TABLE `stock_in`
 -- AUTO_INCREMENT for table `ambil_minyak`
 --
 ALTER TABLE `ambil_minyak`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
 
 --
 -- AUTO_INCREMENT for table `login`
@@ -156,13 +173,13 @@ ALTER TABLE `login`
 -- AUTO_INCREMENT for table `nelayan`
 --
 ALTER TABLE `nelayan`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 
 --
 -- AUTO_INCREMENT for table `stock_in`
 --
 ALTER TABLE `stock_in`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
